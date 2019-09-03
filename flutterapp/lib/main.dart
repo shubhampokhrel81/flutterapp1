8892de18e971c908eca381faa208b4c81eb54979
Sabin Pokhrel <shubhampokhrel81@gmail.com>
2019-09-03 16:22:43 +0545

    Dynamic Lists

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -1,38 +1,9 @@
 import "package:flutter/material.dart";
 
-void main(){
-  runApp(MaterialApp(
-    title: "Exploring UI Widgets",
-    home:Scaffold(
-      appBar: AppBar(title:Text("Widgets List"))  ,
-      body: getListView(),
-    ),
-  ));
-}
+void main() => runApp(MaterialApp(
+  title: "Exploring dynamic lists",
+  home: Scaffold(
+    appBar: AppBar(title:Text("Dynamic lists")),
+  ),
+));
 
-Widget getListView(){
-  var listView = ListView(
-  children: <Widget>[
-    ListTile(
-      leading: Icon(Icons.landscape),
-      title: Text("Landscape",style: TextStyle(color: Colors.blue, fontSize: 20.0),),
-      subtitle: Text("Nepal is beautiful",style:TextStyle(fontSize: 15.0)),
-      trailing: Icon(Icons.wb_sunny),
-      onTap: (){
-        print("hello"); //simply prints hello on console
-      },
-    ),
-    ListTile(
-      leading: Icon(Icons.laptop_chromebook),
-      title: Text("Laptop",style: TextStyle(color: Colors.blue, fontSize: 20.0),),
-    ),
-    ListTile(
-      leading: Icon(Icons.phone),
-      title: Text("Phone",style: TextStyle(color: Colors.blue, fontSize: 20.0),),
-    ),
-    Text("Another element Text on listTile", style:TextStyle(fontSize: 30.0),textAlign: TextAlign.center,),
-    Container(color: Colors.blueGrey,height: 40.0,)
-  ],
-  );
-  return listView;
-}
