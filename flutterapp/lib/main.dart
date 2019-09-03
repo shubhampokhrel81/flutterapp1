1712342282218ef2c7666f17ee30f0803b645032
Sabin Pokhrel <shubhampokhrel81@gmail.com>
2019-09-03 16:09:44 +0545

    List Tile added

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -1,9 +1,36 @@
 import "package:flutter/material.dart";
-import 'app_screens/home.dart';
 
-void main() {
+void main(){
   runApp(MaterialApp(
-    title: "Explore UI Widgets",
-    home: Home(),
+    title: "Exploring UI Widgets",
+    home:Scaffold(
+      appBar: AppBar(title:Text("Widgets List"))  ,
+      body: getListView(),
+    ),
   ));
+}
+
+Widget getListView(){
+  var listView = ListView(
+  children: <Widget>[
+    ListTile(
+      leading: Icon(Icons.landscape),
+      title: Text("Landscape",style: TextStyle(color: Colors.blue, fontSize: 20.0),),
+      subtitle: Text("Nepal is beautiful",style:TextStyle(fontSize: 15.0)),
+      trailing: Icon(Icons.wb_sunny),
+      onTap: (){
+        print("hello"); //simply prints hello on console
+      },
+    ),
+    ListTile(
+      leading: Icon(Icons.laptop_chromebook),
+      title: Text("Laptop",style: TextStyle(color: Colors.blue, fontSize: 20.0),),
+    ),
+    ListTile(
+      leading: Icon(Icons.phone),
+      title: Text("Phone",style: TextStyle(color: Colors.blue, fontSize: 20.0),),
+    ),
+  ],
+  );
+  return listView;
 }
