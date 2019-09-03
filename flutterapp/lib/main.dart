7be6b5080245efe9b0f1265e24eb8c523cec44a5
Sabin Pokhrel <shubhampokhrel81@gmail.com>
2019-09-03 16:33:58 +0545

    Multiple Lists added

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -4,6 +4,25 @@ void main() => runApp(MaterialApp(
   title: "Exploring dynamic lists",
   home: Scaffold(
     appBar: AppBar(title:Text("Dynamic lists")),
+    body: getListView(),
   ),
+
 ));
 
+List<String> getListElements(){
+  var items = List<String>.generate(100, (counter) =>"Item $counter");
+  return items;
+}
+
+Widget getListView(){
+  var listItems = getListElements();
+  
+  var listView = ListView.builder(
+      itemBuilder: (context,index){
+        return ListTile(
+          title: Text(listItems[index]),
+        );
+  }
+  );
+  return listView;
+}
